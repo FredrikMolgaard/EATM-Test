f5cac130e27d931d162ae5fe472496b630dc69de
Fredrik <91fremol@boras.stad.se>
2022-11-10 15:11:52 +0100

    ValidateCustomer-method

--- Program.cs
+++ Program.cs
@@ -1,7 +1,28 @@
-﻿internal class Program
+﻿using Dapper;
+using MySqlConnector;
+
+internal class Program
 {
+    public static string sqlString = "Server=localhost;Database=eatm;Uid=Fredrik;Pwd=123;";
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+
+        void ValidateCustomer()
+        {
+            using (var connection = new MySqlConnection(sqlString))
+            {
+                var searchResult = connection.Query<Customer>($"SELECT ID FROM Customer WHERE ID=1");
+                foreach (Customer c in searchResult)
+                {
+                    if (Convert.ToString(c.Id).Contains("1"))
+                    {
+                        Console.WriteLine(c.Id);
+                    }
+
+                }
+
+            }
+        }
     }
+
 }
