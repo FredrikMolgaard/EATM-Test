624936bbbd0656ac1186d3c65682e614277807a2
Jesper Svensson <jesper-svensson@hotmail.com>
2022-11-14 15:07:21 +0100

    edit error

--- GUI/InjectMenu.cs
+++ GUI/InjectMenu.cs
@@ -9,7 +9,7 @@ public class Menu
         Console.WriteLine("Skriv in ID: ");
         string? search = Console.ReadLine();
         var searchResult = db.connection.Query<Debitcard>($"SELECT d.card_number, d.bank_name, d.expiration_date, d.cvc_number, d.pin_number, d.account_id, c.name FROM debitcard d INNER JOIN customer c ON d.customer_id = c.ID WHERE d.ID ='{search}'");
-        Debitcard insertedDebitCard = null;
+        Debitcard insertedDebitCard = null!;
 
         foreach (Debitcard d in searchResult)
         {
