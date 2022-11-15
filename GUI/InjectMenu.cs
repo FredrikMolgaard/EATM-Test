5ade0e6b6574622b6f9341eb8e70b941a94c036c
Fredrik <91fremol@skola.boras.se>
2022-11-15 14:04:55 +0100

    ID.COUNT

--- GUI/InjectMenu.cs
+++ GUI/InjectMenu.cs
@@ -4,15 +4,18 @@ using MySqlConnector;
 public class Menu
 {
     DataBaseConnections db = new();
-    string debitCardHolder;
+    string? debitCardHolder;
+    Debitcard insertedDebitCard = null!;
     public void InjectCard()
-    {   
+    {
         // Console.WriteLine("Injecting card.......... Please Wait");
         // Thread.Sleep(5000);
         Console.WriteLine("Skriv in ID: ");
         string? search = Console.ReadLine();
+        var numberOfUsers = db.connection.QuerySingle<Debitcard>("SELECT COUNT(ID) FROM debitcard;");   // HÄMTAR UT ANTALET BANKKORT I DB. Använd resultat för att se till att man inte kan skriva in ID på ej-existerande bankkort.
+
+
         var searchResult = db.connection.Query<Debitcard>($"SELECT d.card_number, d.bank_name, d.expiration_date, d.cvc_number, d.pin_number, d.account_id, c.name FROM debitcard d INNER JOIN customer c ON d.customer_id = c.ID WHERE d.ID ='{search}'");
-        Debitcard insertedDebitCard = null!;
 
         foreach (Debitcard d in searchResult)
         {
@@ -57,60 +60,62 @@ public class Menu
                 Environment.Exit(0);
             }
 
-            
-            
-            {bool menu = true;
-            while (menu == true)
+
+
             {
-                Console.Clear();
-                Console.WriteLine("[1] - Show Balance\n[2] - Withdraw money\n[3] - Transaction history\n[4] - Exit");
-                ConsoleKey menuKey = Console.ReadKey().Key;
-                
-
-                if (menuKey == ConsoleKey.D1)
+                bool menu = true;
+                while (menu == true)
                 {
                     Console.Clear();
-                    var balanceResult = db.connection.QuerySingle<Account>($"SELECT a.balance FROM account a WHERE a.ID ='{insertedDebitCard.account_id}'");
-                    Console.WriteLine("Balance result:" + balanceResult.balance);
-                    Console.ReadLine();
-                }
-                if (menuKey == ConsoleKey.D2)
-                {
-                    Console.Clear();
-                    Console.WriteLine("How much you want to withdraw?: ");
-                    int moneyToWithdraw = Convert.ToInt32(Console.ReadLine());
-                    int maxAmount = 5000;
-                    var balanceResult = db.connection.QuerySingle<Account>($"SELECT balance FROM account WHERE ID ='{insertedDebitCard.account_id}'");
-                    Console.WriteLine("Balance result:" + balanceResult.balance);
-                   
-                    if (moneyToWithdraw > maxAmount)
-                    {
-                        Console.WriteLine("Your limit for each withdrawl is 5000.");
-                        Thread.Sleep(3000);
-                    }
+                    Console.WriteLine("[1] - Show Balance\n[2] - Withdraw money\n[3] - Transaction history\n[4] - Exit");
+                    ConsoleKey menuKey = Console.ReadKey().Key;
 
-                    else if (balanceResult.balance >= moneyToWithdraw)
+
+                    if (menuKey == ConsoleKey.D1)
                     {
-                        var withdrawResult = db.connection.Query<Account>($"UPDATE account SET balance = '{balanceResult.balance - moneyToWithdraw}' WHERE ID ='{insertedDebitCard.account_id}'");
-                    }
-                    else
-                    {
-                        Console.WriteLine($"Insufficient funds. You have {balanceResult.balance}kr in your account.\nPress any key to return to menu...");
+                        Console.Clear();
+                        var balanceResult = db.connection.QuerySingle<Account>($"SELECT a.balance FROM account a WHERE a.ID ='{insertedDebitCard.account_id}'");
+                        Console.WriteLine("Balance result:" + balanceResult.balance);
                         Console.ReadLine();
                     }
+                    if (menuKey == ConsoleKey.D2)
+                    {
+                        while (true)
+                        {
+                            Console.Clear();
+                            Console.WriteLine("How much you want to withdraw?: ");
+                            int moneyToWithdraw = Convert.ToInt32(Console.ReadLine());
+                            int maxAmount = 5000;
+                            var balanceResult = db.connection.QuerySingle<Account>($"SELECT balance FROM account WHERE ID ='{insertedDebitCard.account_id}'");
+                            Console.WriteLine("Balance result:" + balanceResult.balance);
+
+                            if (moneyToWithdraw > maxAmount)
+                            {
+                                Console.WriteLine("Your limit for each withdrawl is 5000.\nRedirecting...");
+                                Thread.Sleep(3000);
+                            }
+
+                            else if (balanceResult.balance >= moneyToWithdraw)
+                            {
+                                var withdrawResult = db.connection.Query<Account>($"UPDATE account SET balance = '{balanceResult.balance - moneyToWithdraw}' WHERE ID ='{insertedDebitCard.account_id}'");
+                                break;
+                            }
+                            else
+                            {
+                                Console.WriteLine($"Insufficient funds. You have {balanceResult.balance}kr in your account.\nPress any key to return to menu...");
+                                Console.ReadLine();
+                            }
+                        }
+
+                    }
+                    if (menuKey == ConsoleKey.D4)
+                    {
+                        Console.Clear();
+                        Console.WriteLine("Ejecting card.......");
+                        Thread.Sleep(2500);
+                        Environment.Exit(0);
+                    }
                 }
-                if (menuKey == ConsoleKey.D4)
-                {
-                    Console.Clear();
-                    Console.WriteLine("Ejecting card.......");
-                    Thread.Sleep(2500);
-                    Console.WriteLine("Din mamma är en mullvaden");
-                    Thread.Sleep(2500);
-                    Console.WriteLine("Hejdå sonen till mamma mullvad");
-                    Thread.Sleep(2500);
-                    Environment.Exit(0);
-                }
-            }
             }
         }
     }
